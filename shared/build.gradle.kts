1cfd339489da2e53563c2f61c3f35d6853a3b6f7
andb3 <andb3003@yahoo.com>
2021-03-31 14:55:11 -0400

    Add iOS target

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("multiplatform")
     kotlin("plugin.serialization")
+    id("org.jetbrains.kotlin.native.cocoapods")
 }
 
 group = "com.andb.com.andb.apps"
@@ -19,14 +20,30 @@ kotlin {
     js(BOTH) {
         browser {}
     }
+    val sdkName: String? = System.getenv("SDK_NAME")
+    val isiOSDevice = sdkName.orEmpty().startsWith("iphoneos")
+    if (isiOSDevice) {
+        iosArm64("iOS")
+    } else {
+        iosX64("iOS")
+    }
+
+    cocoapods {
+        // Configure fields required by CocoaPods.
+        summary = "Some description for a Kotlin/Native module"
+        homepage = "Link to a Kotlin/Native module homepage"
+    }
+
 
     sourceSets {
         val commonMain by getting {
             dependencies {
                 implementation(kotlin("stdlib-common"))
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3-native-mt") {
+                    isForce = true
+                }
                 implementation ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
-                implementation("com.adamratzman:spotify-api-kotlin-core:3.6.0-rc.2")
+                implementation("com.adamratzman:spotify-api-kotlin-core:3.6.03")
                 implementation("io.ktor:ktor-client-core:1.5.0")
                 implementation("io.ktor:ktor-client-serialization:1.5.0")
             }
@@ -57,5 +74,9 @@ kotlin {
         }
 
         val jsTest by getting {}
+
+/*        val iOSMain by getting {
+        }
+        val iOSTest by getting{}*/
     }
 }
