7158c44742fa37fc8f0019fe256f3550f7fdc046
andb3 <andb3003@yahoo.com>
2021-08-14 21:25:49 -0400

    Add Lyrical icon to window instead of default swing icon

--- ui/src/jvmMain/kotlin/Main.kt
+++ ui/src/jvmMain/kotlin/Main.kt
@@ -1,5 +1,10 @@
-import androidx.compose.desktop.Window
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
 
-fun main() = Window(title = "Lyrical") {
-    App()
+fun main() = application {
+    Window(onCloseRequest = ::exitApplication, title = "Lyrical", icon = painterResource("LyricalIcon.svg")) {
+        App()
+    }
 }
+
